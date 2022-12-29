5ca183a6368ea3fee06b2d7138522a3d78cafd2a
softBelle <younglady19931114@gmail.com>
2022-12-29 08:22:42 +0600

    1

---
+++ superwarden/frontend/src/store/store.js
@@ -0,0 +1,12 @@
+import { createStore } from 'vuex'
+import getters from './getters'
+import account from './modules/account'
+
+const store = createStore({
+    modules: {
+        account,
+    },
+    getters,
+  })
+  
+  export default store
